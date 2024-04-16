284d3ed98df5968759e735ef96f210273d8df008
Daria Polubenko <daria.polubenko@gmail.com>
2024-04-16 19:24:12 +0700

    update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,17 +1,19 @@
 package hexlet.code;
 
 import picocli.CommandLine;
-import java.util.concurrent.Callable;
 
-@CommandLine.Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
+@CommandLine.Command(
+        name = "gendiff",
+        mixinStandardHelpOptions = true,
+        version = "gendiff 1.0",
         description = "Compares two configuration files and shows a difference.")
-public class App implements Callable<Integer> {
+
+public class App implements Runnable {
     @Override
-    public Integer call() throws Exception { // your business logic goes here...
+    public void run() {
         System.out.println("Hello, World!");
-        return 0;
     }
-    public static void main(String... args) {
+    public static void main(String[] args) {
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
     }
