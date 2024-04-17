1474629218ce72675c50f1aa07ab90f932100c84
Daria Polubenko <daria.polubenko@gmail.com>
2024-04-18 02:12:12 +0700

    update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -6,8 +6,10 @@ import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
 import java.io.File;
-import java.math.BigInteger;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.util.concurrent.Callable;
 
@@ -29,12 +31,19 @@ public class App implements Callable<Integer> {
     private File filepath2;
 
     @Override
-    public Integer call() throws Exception {
+    public Integer call(File filepath1, File filepath2) throws Exception {
         //byte[] fileContents = Files.readAllBytes(filepath1.toPath());
         //byte[] digest = MessageDigest.getInstance(format).digest(fileContents);
         //System.out.printf("%0" + (digest.length*2) + "x%n", new BigInteger(1, digest));
 
-        System.out.println("Hello, World!");
+        Path filePath1 = Paths.get(filepath1).toAbsolutePath().normalize();
+        Path filePath2 = Paths.get(filepath2).toAbsolutePath().normalize();
+
+        String content1 = Files.readString(filePath1);
+        String content2 = Files.readString(filePath2);
+
+        System.out.println(content1);
+        System.out.println(content2);
         return 0;
     }
     public static void main(String... args) {
