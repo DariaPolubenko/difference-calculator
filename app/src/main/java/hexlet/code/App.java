93a5e1b5b48820fdd3f277c03a547b61cf65317a
Daria Polubenko <daria.polubenko@gmail.com>
2024-04-18 03:42:59 +0700

    update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -13,6 +13,9 @@ import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.util.concurrent.Callable;
 
+import java.util.Map;
+import java.util.HashMap;
+
 @CommandLine.Command(
         name = "gendiff",
         mixinStandardHelpOptions = true,
@@ -31,24 +34,41 @@ public class App implements Callable<Integer> {
     private File filepath2;
 
     @Override
-    public Integer call(File filepath1, File filepath2) throws Exception {
+    public Integer call() throws Exception {
         //byte[] fileContents = Files.readAllBytes(filepath1.toPath());
         //byte[] digest = MessageDigest.getInstance(format).digest(fileContents);
         //System.out.printf("%0" + (digest.length*2) + "x%n", new BigInteger(1, digest));
 
-        Path filePath1 = Paths.get(filepath1).toAbsolutePath().normalize();
-        Path filePath2 = Paths.get(filepath2).toAbsolutePath().normalize();
+        Path filePath1 = Paths.get("/Users/new/Desktop/file1.json").toAbsolutePath().normalize();
+        Path filePath2 = Paths.get("/Users/new/Desktop/file2.json").toAbsolutePath().normalize();
 
         String content1 = Files.readString(filePath1);
         String content2 = Files.readString(filePath2);
 
-        System.out.println(content1);
-        System.out.println(content2);
+        System.out.println(getData(content1));
+        System.out.println(getData(content2));
+        //System.out.println(content2);
         return 0;
     }
-    public static void main(String... args) {
+
+    public static Map getData(String content) throws Exception {
+        var cutСontent  = content.replace("{", "")
+                .replace("}", "");
+
+        String[] data = cutСontent.split(",");
+        Map<String, String> dataMap = new HashMap<>();
+
+        for (var dataKeyValue : data) {
+            String[] keyValue = dataKeyValue.split(":");
+            dataMap.put(keyValue[0], keyValue[1]);
+        }
+        return dataMap;
+    }
+
+    public static void main(String[] args) {
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
     }
 
 }
+
