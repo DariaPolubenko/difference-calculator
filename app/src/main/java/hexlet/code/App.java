27fc9d712adfb9cc0a78b6ccb6afc7ac1ee83174
Daria Polubenko <daria.polubenko@gmail.com>
2024-04-17 20:40:26 +0700

    update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,6 +1,14 @@
 package hexlet.code;
 
 import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.io.File;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.security.MessageDigest;
 import java.util.concurrent.Callable;
 
 @CommandLine.Command(
@@ -9,9 +17,23 @@ import java.util.concurrent.Callable;
         version = "gendiff 1.0",
         description = "Compares two configuration files and shows a difference.")
 
+
 public class App implements Callable<Integer> {
+    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]", paramLabel = "format")
+    private String format = "stylish";
+
+    @Parameters(index = "0", description = "path to first file", paramLabel = "filepath1")
+    private File filepath1;
+
+    @Parameters(index = "1", description = "path to second file", paramLabel = "filepath2")
+    private File filepath2;
+
     @Override
     public Integer call() throws Exception {
+        //byte[] fileContents = Files.readAllBytes(filepath1.toPath());
+        //byte[] digest = MessageDigest.getInstance(format).digest(fileContents);
+        //System.out.printf("%0" + (digest.length*2) + "x%n", new BigInteger(1, digest));
+
         System.out.println("Hello, World!");
         return 0;
     }
@@ -21,3 +43,47 @@ public class App implements Callable<Integer> {
     }
 
 }
+
+/*
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.io.File;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.security.MessageDigest;
+import java.util.concurrent.Callable;
+
+
+@CommandLine.Command(
+        name = "gendiff",
+        mixinStandardHelpOptions = true,
+        version = "gendiff 1.0",
+        description = "Compares two configuration files and shows a difference.")
+
+class App implements Callable<Integer> {
+
+    @Parameters(index = "0", description = "path to first file")
+    private File file;
+
+    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
+    private String format = "SHA-256";
+
+    @Override
+    public Integer call() throws Exception { // your business logic goes here...
+        //byte[] fileContents = Files.readAllBytes(file.toPath());
+        //byte[] digest = MessageDigest.getInstance(format).digest(fileContents);
+        //System.out.printf("%0" + (digest.length*2) + "x%n", new BigInteger(1, digest));
+        System.out.println("Hello, World");
+        return 0;
+    }
+
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+}
+ */
+
