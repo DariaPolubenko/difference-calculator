1c8fe07a6d0c86933d7ec37efdce2658d847469a
Daria Polubenko <daria.polubenko@gmail.com>
2024-04-19 03:04:36 +0700

    update Differ.java

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -2,26 +2,31 @@ package hexlet.code;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+
+
 
 public class Differ {
     public static Map generate(Map data1, Map data2) {
 
-        Map result = new HashMap<>();
-        //var result = new StringBuilder();
+        //Map result = new HashMap<>();
+        var result = new StringBuilder();
 
-        // знаю, что цикл в цикле - решение merda, но пока не понимаю
-        // как решить оптимальнее, не в лоб
+        // знаю, что цикл в цикле - решение merda, и решение выглядит супер сложно,
+        // но пока не понимаю, как решить оптимальнее, не в лоб
         // возможно, тут могут сработать функции/стримы, но х(
 
         for (var key1 : data1.keySet()) {
 
             for (var key2 : data2.keySet()) {
 
-                if (data1.containsKey(key2)) {
+                if (key1.equals(key2)) {
                     var value1 = data1.get(key1);
                     var value2 = data2.get(key2);
 
                     if (value1.equals(value2)) {
+                        result.append("<li>");
                         result.put("  " + key1, value1);
                     } else {
                         result.put("- " + key1, value1);
@@ -34,7 +39,12 @@ public class Differ {
                 }
             }
         }
-        return result;
+        var sortedMap = new TreeMap<>(result);
+
+        //var sortedResult = result.entrySet().stream()
+                //.sorted()
+
+        return sortedMap;
     }
 }
 
