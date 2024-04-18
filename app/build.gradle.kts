f1517c80867c375c026a7d181eb98daa390c72d1
Daria Polubenko <daria.polubenko@gmail.com>
2024-04-18 23:19:04 +0700

    update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,6 +18,8 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("info.picocli:picocli:4.7.5")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.0")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.13.0")
 }
 
 tasks.test {
