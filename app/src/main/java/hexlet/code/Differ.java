902bf31d99706b338cfeca8093d4784eb2e48af1
Daria Polubenko <daria.polubenko@gmail.com>
2024-04-18 23:19:57 +0700

    add Differ.java

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,2 +1,35 @@
-package hexlet.code;public class Differ {
+package hexlet.code;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Differ {
+   public static String generate(Map data1, Map data2) {
+
+       Map result = new HashMap<>(data1);
+
+       //var keysOfData1 = data1.keySet();
+       //var result = new StringBuilder();
+
+       var keys = data2.keySet();
+       for (var key : keys) {
+           result.compute(key, (key, count) -> {
+               count == null ? 1 : count + 1);
+
+               }
+           }
+
+       }
+
+
+
+
+
+
+
+
+       return str;
+
+
+    }
 }
