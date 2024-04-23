4b162c6404d0855aae3466de81291b3e6374e5b4
Daria Polubenko <daria.polubenko@gmail.com>
2024-04-23 20:37:10 +0700

    update Differ.java

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,27 +1,23 @@
 package hexlet.code;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.stream.Collectors;
-
-
 
 public class Differ {
     public static String generate(Map data1, Map data2) {
 
-        var firstSorted = new TreeMap<>(data1);
-        var unitedMap = new TreeMap<>(data2);
-        firstSorted.forEach((key, value) -> {
-            if (!unitedMap.containsKey(key)) {
-                unitedMap.put(key, value);
+        var sortedData1 = new TreeMap<>(data1);
+        var unitedData = new TreeMap<>(data2);
+
+        sortedData1.forEach((key, value) -> {
+            if (!unitedData.containsKey(key)) {
+                unitedData.put(key, value);
             }
         });
 
         var result = new StringBuilder();
         result.append("{\n");
-        unitedMap.forEach((key, value) -> {
+        unitedData.forEach((key, value) -> {
             if (data1.containsKey(key) && data2.containsKey(key)) {
                 if (data1.get(key).equals(data2.get(key))) {
                     result.append("   " + key + ": " + value + "\n");
@@ -31,7 +27,7 @@ public class Differ {
                 }
             } else if (!data1.containsKey(key)) {
                 result.append(" + " + key + ": " + data2.get(key) + "\n");
-            }else if (!data2.containsKey(key)) {
+            } else if (!data2.containsKey(key)) {
                 result.append(" - " + key + ": " + data1.get(key) + "\n");
             }
         });
