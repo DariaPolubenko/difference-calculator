fa81a047fcfe1f2ebcae702bb5869465fef93f30
Daria Polubenko <daria.polubenko@gmail.com>
2024-04-24 22:15:12 +0700

    update AppTest.java

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -1,7 +1,7 @@
 package hexlet.code;
 
 import static hexlet.code.App.getData;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 import java.nio.file.Path;
@@ -9,7 +9,7 @@ import java.nio.file.Paths;
 
 class AppTest {
     @Test
-    public void Test1() throws Exception {
+    public void test1() throws Exception {
 
         Path filepath1 = Paths.get("file1.json");
         Path filepath2 = Paths.get("file2.json");
@@ -18,19 +18,19 @@ class AppTest {
         var data2 = getData(filepath2);
 
         var actual = Differ.generate(data1, data2);
-        var expected = "{\n" +
-                       "  - follow: false\n" +
-                       "    host: hexlet.io\n" +
-                       "  - proxy: 123.234.53.22\n" +
-                       "  - timeout: 50\n" +
-                       "  + timeout: 20\n" +
-                       "  + verbose: true\n" +
-                       "}";
+        var expected = "{\n"
+                       + "  - follow: false\n"
+                       + "    host: hexlet.io\n"
+                       + "  - proxy: 123.234.53.22\n"
+                       + "  - timeout: 50\n"
+                       + "  + timeout: 20\n"
+                       + "  + verbose: true\n"
+                       + "}";
         assertEquals(expected, actual);
     }
 
     @Test
-    public void Test2() throws Exception {
+    public void test2() throws Exception {
 
         Path filepath1 = Paths.get("file21.json");
         Path filepath2 = Paths.get("file22.json");
@@ -39,38 +39,38 @@ class AppTest {
         var data2 = getData(filepath2);
 
         var actual = Differ.generate(data1, data2);
-        var expected = "{\n" +
-                "  - Возраст: 25\n" +
-                "  - Город: Сочи\n" +
-                "  - Имя: Иван\n" +
-                "  + Имя: Илья\n" +
-                "  + Образование: НГТУ\n" +
-                "    Фамилия: Ужиков\n" +
-                "}";
+        var expected = "{\n"
+                + "  - Возраст: 25\n"
+                + "  - Город: Сочи\n"
+                + "  - Имя: Иван\n"
+                + "  + Имя: Илья\n"
+                + "  + Образование: НГТУ\n"
+                + "    Фамилия: Ужиков\n"
+                + "}";
         assertEquals(expected, actual);
     }
 
     @Test
-    public void Test3() throws Exception {
+    public void test3() throws Exception {
 
-        Path filepath1 = Paths.get("file31.json");
-        Path filepath2 = Paths.get("file32.json");
+        Path filepath1 = Paths.get("file3.json");
+        Path filepath2 = Paths.get("file21.json");
 
         var data1 = getData(filepath1);
         var data2 = getData(filepath2);
 
         var actual = Differ.generate(data1, data2);
-        var expected = "{\n" +
-                "  + Возраст: 25\n" +
-                "  + Город: Сочи\n" +
-                "  + Имя: Иван\n" +
-                "  + Фамилия: Ужиков\n" +
-                "}";
+        var expected = "{\n"
+                + "  + Возраст: 25\n"
+                + "  + Город: Сочи\n"
+                + "  + Имя: Иван\n"
+                + "  + Фамилия: Ужиков\n"
+                + "}";
         assertEquals(expected, actual);
     }
 
     @Test
-    public void Test4() throws Exception {
+    public void test4() throws Exception {
 
         Path filepath1 = Paths.get("file3.json"); //пустой
         Path filepath2 = Paths.get("file21.json");
@@ -79,17 +79,17 @@ class AppTest {
         var data2 = getData(filepath2);
 
         var actual = Differ.generate(data1, data2);
-        var expected = "{\n" +
-                "  + Возраст: 25\n" +
-                "  + Город: Сочи\n" +
-                "  + Имя: Иван\n" +
-                "  + Фамилия: Ужиков\n" +
-                "}";
+        var expected = "{\n"
+                + "  + Возраст: 25\n"
+                + "  + Город: Сочи\n"
+                + "  + Имя: Иван\n"
+                + "  + Фамилия: Ужиков\n"
+                + "}";
         assertEquals(expected, actual);
     }
 
     @Test
-    public void Test5() throws Exception {
+    public void test5() throws Exception {
 
         Path filepath1 = Paths.get("file21.json");
         Path filepath2 = Paths.get("file3.json"); //пустой
@@ -98,12 +98,12 @@ class AppTest {
         var data2 = getData(filepath2);
 
         var actual = Differ.generate(data1, data2);
-        var expected = "{\n" +
-                "  - Возраст: 25\n" +
-                "  - Город: Сочи\n" +
-                "  - Имя: Иван\n" +
-                "  - Фамилия: Ужиков\n" +
-                "}";
+        var expected = "{\n"
+                + "  - Возраст: 25\n"
+                + "  - Город: Сочи\n"
+                + "  - Имя: Иван\n"
+                + "  - Фамилия: Ужиков\n"
+                + "}";
         assertEquals(expected, actual);
     }
 }
