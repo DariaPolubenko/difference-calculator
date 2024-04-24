937cbe6606c6fe1f614df8c5735edc5060c4e186
Daria Polubenko <daria.polubenko@gmail.com>
2024-04-25 03:31:38 +0700

    update AppTest.java

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -1,24 +1,17 @@
 package hexlet.code;
 
-import static hexlet.code.App.getData;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
-
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 class AppTest {
     @Test
     public void test1() throws Exception {
 
-        Path filepath1 = Paths.get("file1.json");
-        Path filepath2 = Paths.get("file2.json");
+        var data1 = App.getData("src/test/resources/file1.json");
+        var data2 = App.getData("src/test/resources/file2.json");
 
-        var data1 = getData("src/test/resources/" + filepath1);
-        var data2 = getData("src/test/resources/" + filepath2);
-
-        var actual = Differ.generate(data1, data2);
-        var expected = "{\n"
+        String actual = Differ.generate(data1, data2);
+        String expected = "{\n"
                        + "  - follow: false\n"
                        + "    host: hexlet.io\n"
                        + "  - proxy: 123.234.53.22\n"
@@ -32,11 +25,8 @@ class AppTest {
     @Test
     public void test2() throws Exception {
 
-        Path filepath1 = Paths.get("file21.json");
-        Path filepath2 = Paths.get("file22.json");
-
-        var data1 = getData("src/test/resources/" + filepath1);
-        var data2 = getData("src/test/resources/" + filepath2);
+        var data1 = App.getData("src/test/resources/file21.json");
+        var data2 = App.getData("src/test/resources/file22.json");
 
         var actual = Differ.generate(data1, data2);
         var expected = "{\n"
@@ -53,11 +43,8 @@ class AppTest {
     @Test
     public void test3() throws Exception {
 
-        Path filepath1 = Paths.get("file3.json");
-        Path filepath2 = Paths.get("file21.json");
-
-        var data1 = getData("src/test/resources/" + filepath1);
-        var data2 = getData("src/test/resources/" + filepath2);
+        var data1 = App.getData("src/test/resources/file3.json");
+        var data2 = App.getData("src/test/resources/file21.json");
 
         var actual = Differ.generate(data1, data2);
         var expected = "{\n"
@@ -72,11 +59,8 @@ class AppTest {
     @Test
     public void test4() throws Exception {
 
-        Path filepath1 = Paths.get("file3.json"); //пустой
-        Path filepath2 = Paths.get("file21.json");
-
-        var data1 = getData("src/test/resources/" + filepath1);
-        var data2 = getData("src/test/resources/" + filepath2);
+        var data1 = App.getData("src/test/resources/file3.json"); //пустой
+        var data2 = App.getData("src/test/resources/file21.json");
 
         var actual = Differ.generate(data1, data2);
         var expected = "{\n"
@@ -91,11 +75,8 @@ class AppTest {
     @Test
     public void test5() throws Exception {
 
-        Path filepath1 = Paths.get("file21.json");
-        Path filepath2 = Paths.get("file3.json"); //пустой
-
-        var data1 = getData("src/test/resources/" + filepath1);
-        var data2 = getData("src/test/resources/" + filepath2);
+        var data1 = App.getData("src/test/resources/file21.json");
+        var data2 = App.getData("src/test/resources/file3.json"); //пустой
 
         var actual = Differ.generate(data1, data2);
         var expected = "{\n"
