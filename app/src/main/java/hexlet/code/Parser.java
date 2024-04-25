6833637a76758d09f7abdb7c44fa9498540a0245
Daria Polubenko <daria.polubenko@gmail.com>
2024-04-25 20:26:16 +0700

    update Parser.java

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -2,6 +2,7 @@ package hexlet.code;
 
 import java.util.Map;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 
 public class Parser {
     public static Map<String, Object> parseJson(String content) throws Exception {
@@ -9,6 +10,14 @@ public class Parser {
             return Map.of("", "");
         }
         ObjectMapper parseContent = new ObjectMapper();
+        return parseContent.readValue(content, Map.class);
+    }
+
+    public static Map<String, Object> parseYaml(String content) throws Exception {
+        if (content.isEmpty()) {
+            return Map.of("", "");
+        }
+        ObjectMapper parseContent = new YAMLMapper();
         return parseContent.readValue(content, Map.class);
     }
 }
