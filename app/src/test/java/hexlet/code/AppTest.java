6c760f558e5acd20c92e88bc31854917079eebac
Daria Polubenko <daria.polubenko@gmail.com>
2024-04-28 19:41:24 +0700

    update AppTest.java

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -10,8 +10,8 @@ class AppTest {
         var data1 = App.getData("src/test/resources/file1.json");
         var data2 = App.getData("src/test/resources/file2.json");
 
-        String actual = Differ.generate(data1, data2);
-        String expected = "{\n"
+        var actual = Differ.generate(data1, data2);
+        var expected = "{\n"
                        + "  - follow: false\n"
                        + "    host: hexlet.io\n"
                        + "  - proxy: 123.234.53.22\n"
@@ -43,7 +43,7 @@ class AppTest {
     @Test
     public void test3() throws Exception {
 
-        var data1 = App.getData("src/test/resources/file3.json");
+        var data1 = App.getData("src/test/resources/file3.json"); //пустой
         var data2 = App.getData("src/test/resources/file21.json");
 
         var actual = Differ.generate(data1, data2);
@@ -59,22 +59,6 @@ class AppTest {
     @Test
     public void test4() throws Exception {
 
-        var data1 = App.getData("src/test/resources/file3.json"); //пустой
-        var data2 = App.getData("src/test/resources/file21.json");
-
-        var actual = Differ.generate(data1, data2);
-        var expected = "{\n"
-                + "  + Возраст: 25\n"
-                + "  + Город: Сочи\n"
-                + "  + Имя: Иван\n"
-                + "  + Фамилия: Ужиков\n"
-                + "}";
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void test5() throws Exception {
-
         var data1 = App.getData("src/test/resources/file21.json");
         var data2 = App.getData("src/test/resources/file3.json"); //пустой
 
@@ -84,6 +68,36 @@ class AppTest {
                 + "  - Город: Сочи\n"
                 + "  - Имя: Иван\n"
                 + "  - Фамилия: Ужиков\n"
+                + "}";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test5() throws Exception {
+
+        var data1 = App.getData("src/test/resources/file3.json"); //пустой
+        var data2 = App.getData("src/test/resources/file4.json"); //пустой
+
+        var actual = Differ.generate(data1, data2);
+        var expected = "{\n"
+                + "}";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test6() throws Exception {
+
+        var data1 = App.getData("src/test/resources/file1.yml");
+        var data2 = App.getData("src/test/resources/file2.yml");
+
+        var actual = Differ.generate(data1, data2);
+        var expected = "{\n"
+                + "  - follow: false\n"
+                + "    host: hexlet.io\n"
+                + "  - proxy: 123.234.53.22\n"
+                + "  - timeout: 50\n"
+                + "  + timeout: 20\n"
+                + "  + verbose: true\n"
                 + "}";
         assertEquals(expected, actual);
     }
