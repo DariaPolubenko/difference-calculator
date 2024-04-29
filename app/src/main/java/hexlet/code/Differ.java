edb86a71f42a3f87738fa202afe333890527658a
Daria Polubenko <daria.polubenko@gmail.com>
2024-04-30 04:22:34 +0700

    update Differ.java

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,37 +1,75 @@
 package hexlet.code;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
-import java.util.Objects;
+import java.util.HashMap;
 import java.util.TreeMap;
+import java.util.Objects;
 
 public class Differ {
     public static String generate(Map<String, Object> data1, Map<String, Object> data2) {
-        var sortedData1 = new TreeMap<>(data1);
-        var unitedData = new TreeMap<>(data2);
+        return generate(data1, data2, "stylish");
+    }
 
-        sortedData1.forEach((key, value) -> {
-            unitedData.computeIfAbsent(key, (v) -> value);
+    public static String generate(Map<String, Object> data1, Map<String, Object> data2, String format) {
+        List<Map<String, Object>> differences = getDifference(data1, data2);
+
+        var result = "";
+        if (format.equals("stylish")) {
+            result = Formatter.stylish(differences);
+        }
+        return result;
+    }
+
+    public static List<Map<String, Object>> getDifference(Map<String, Object> data1, Map<String, Object> data2) {
+        var unitedKeys = new TreeMap<>(data2);
+        data1.forEach((key, value) -> {
+            unitedKeys.computeIfAbsent(key, (v) -> value);
         });
-        var result = new StringBuilder();
-        result.append("{\n");
+        var result = new ArrayList<Map<String, Object>>();
 
-        unitedData.forEach((key, value) -> {
+        unitedKeys.forEach((key, value) -> {
+            var helperMap = new HashMap<String, Object>();
+            var value1 = data1.get(key);
+            var value2 = data2.get(key);
+
             if (key.equals("")) {
-                result.append("");
+                result.add(Map.of("", ""));
+
             } else if (data1.containsKey(key) && data2.containsKey(key)) {
-                if (Objects.equals(data1.get(key), data2.get(key))) {
-                    result.append("    " + key + ": " + data1.get(key) + "\n");
+                if (Objects.equals(value1, value2)) {
+                    helperMap.put("key", key);
+                    helperMap.put("type", "unchanged");
+                    helperMap.put("old value", value1);
+                    helperMap.put("new value", value1);
+
+                    result.add(helperMap);
                 } else {
-                    result.append("  - " + key + ": " + data1.get(key) + "\n");
-                    result.append("  + " + key + ": " + data2.get(key) + "\n");
+                    helperMap.put("key", key);
+                    helperMap.put("type", "changed");
+                    helperMap.put("old value", value1);
+                    helperMap.put("new value", value2);
+
+                    result.add(helperMap);
                 }
             } else if (!data1.containsKey(key)) {
-                result.append("  + " + key + ": " + data2.get(key) + "\n");
+                helperMap.put("key", key);
+                helperMap.put("type", "added");
+                helperMap.put("old value", "");
+                helperMap.put("new value", value2);
+
+                result.add(helperMap);
+
             } else if (!data2.containsKey(key)) {
-                result.append("  - " + key + ": " + data1.get(key) + "\n");
+                helperMap.put("key", key);
+                helperMap.put("type", "deleted");
+                helperMap.put("old value", value1);
+                helperMap.put("new value", "");
+
+                result.add(helperMap);
             }
         });
-        result.append("}");
-        return result.toString();
+        return result;
     }
 }
