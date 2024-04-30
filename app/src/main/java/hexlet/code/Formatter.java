6b731e507d8c3fa64e32c7833499ee19048d3bcb
Daria Polubenko <daria.polubenko@gmail.com>
2024-04-30 21:20:43 +0700

    update Formatter.java

--- app/src/main/java/hexlet/code/Formatter.java
+++ app/src/main/java/hexlet/code/Formatter.java
@@ -1,33 +1,17 @@
 package hexlet.code;
 
+import formatters.Plain;
+import formatters.Stylish;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 public class Formatter {
-    public static String stylish(List<Map<String, Object>> data) {
-        var result = new StringBuilder();
-        result.append("{\n");
+    public static String formatter(List<Map<String, Object>> differences, String format) {
 
-        for (var map : data) {
-            map.forEach((key, value) -> {
-                var oldValue = map.get("old value");
-                var newValue = map.get("new value");
-                if (key.equals("")) {
-                    result.append("");
-                } else if (Objects.equals(map.get(key), "unchanged")) {
-                    result.append("    " + map.get("key") + ": " + oldValue + "\n");
-                } else if (Objects.equals(map.get(key), "changed")) {
-                    result.append("  - " + map.get("key") + ": " + oldValue + "\n");
-                    result.append("  + " + map.get("key") + ": " + newValue + "\n");
-                } else if (Objects.equals(map.get(key), "added")) {
-                    result.append("  + " + map.get("key") + ": " + newValue + "\n");
-                } else if (Objects.equals(map.get(key), "deleted")) {
-                    result.append("  - " + map.get("key") + ": " + oldValue + "\n");
-                }
-            });
-        }
-        result.append("}");
-        return result.toString();
+        var result = format.equals("stylish") ? Stylish.formatter(differences)
+                : format.equals("plain") ? Plain.formatter(differences)
+                : "Don't have format " + format;
+
+        return result;
     }
 }
