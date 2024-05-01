b15305d3ebd36834da521425595000e1361a45dd
Daria Polubenko <daria.polubenko@gmail.com>
2024-05-01 18:06:12 +0700

    update Formatter.java

--- app/src/main/java/hexlet/code/Formatter.java
+++ app/src/main/java/hexlet/code/Formatter.java
@@ -1,15 +1,17 @@
 package hexlet.code;
 
+import formatters.Json;
 import formatters.Plain;
 import formatters.Stylish;
 import java.util.List;
 import java.util.Map;
 
 public class Formatter {
-    public static String formatter(List<Map<String, Object>> differences, String format) {
+    public static String formatter(List<Map<String, Object>> differences, String format) throws Exception {
 
         var result = format.equals("stylish") ? Stylish.formatter(differences)
                 : format.equals("plain") ? Plain.formatter(differences)
+                : format.equals("json") ? Json.formatter(differences)
                 : "Don't have format " + format;
 
         return result;
