562245d86c767583bd38785dc1cc91b77e50e5ec
Daria Polubenko <daria.polubenko@gmail.com>
2024-05-02 20:17:43 +0700

    update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,17 +3,10 @@ package hexlet.code;
 import picocli.CommandLine;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
-//import picocli.CommandLine.Command;
 
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-
 import java.util.concurrent.Callable;
-import java.util.Map;
-//import java.math.BigInteger;
-//import java.security.MessageDigest;
-//import java.sql.SQLOutput;
+
 
 @CommandLine.Command(
         name = "gendiff",
@@ -37,30 +30,9 @@ public class App implements Callable<Integer> {
 
     @Override
     public Integer call() throws Exception {
-        var data1 = getData("src/test/resources/" + filepath1);
-        var data2 = getData("src/test/resources/" + filepath2);
-
-        var diff = Differ.generate(data1, data2, format);
+        var diff = Differ.generate(filepath1, filepath2, format);
         System.out.println(diff);
-
         return 0;
-    }
-
-    public static Map<String, Object> getData(String filepath) throws Exception {
-        var fullPath = Paths.get(filepath).toAbsolutePath().normalize();
-
-        if (!Files.exists(fullPath)) {
-            throw new Exception("File '" + fullPath + "' does not exist");
-        }
-
-        String[] format = filepath.split("\\.");
-        if (format[1].equals("json")) {
-            var content = Files.readString(fullPath);
-            return Parser.parseJson(content);
-        } else {
-            var content = Files.readString(fullPath);
-            return Parser.parseYaml(content);
-        }
     }
 
     public static void main(String[] args) {
