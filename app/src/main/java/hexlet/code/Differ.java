2f0b18016ccd692a7e17572e5b431fc2b0d62e8d
Daria Polubenko <daria.polubenko@gmail.com>
2024-05-10 01:00:57 +0700

    update Differ.java

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -2,6 +2,7 @@ package hexlet.code;
 
 import static hexlet.code.Formatter.formatter;
 import static hexlet.code.Tree.getTree;
+import static hexlet.code.Content.formatContent;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -14,8 +15,8 @@ public class Differ {
     }
 
     public static String generate(String filepath1,  String filepath2, String format) throws Exception {
-        filepath1 = filepath1.contains("/") ? filepath1 : "src/test/resources/" + filepath1;
-        filepath2 = filepath2.contains("/") ? filepath2 : "src/test/resources/" + filepath2;
+        filepath1 = filepath1.contains("src/test/resources/") ? filepath1 : "src/test/resources/" + filepath1;
+        filepath2 = filepath2.contains("src/test/resources/") ? filepath2 : "src/test/resources/" + filepath2;
 
         var data1 = getData(filepath1);
         var data2 = getData(filepath2);
@@ -31,16 +32,8 @@ public class Differ {
             throw new Exception("File '" + fullPath + "' does not exist");
         }
         var content = Files.readString(fullPath);
+        var result = formatContent(filepath, content);
 
-        String[] format = filepath.split("\\.");
-        switch (format[1]) {
-            case "json":
-                return Parser.parseJson(content);
-            case "yml":
-                return Parser.parseYaml(content);
-            default:
-                System.out.println("Don't have this format: \"" + format[1] + "\"");
-        }
-        return null;
+        return result;
     }
 }
