50cb0f0504189f8e6c1a27ccf94f54b458896153
Daria Polubenko <daria.polubenko@gmail.com>
2024-05-10 03:26:58 +0700

    update Plain.java

--- app/src/main/java/formatters/Plain.java
+++ app/src/main/java/formatters/Plain.java
@@ -6,7 +6,7 @@ import java.util.Map;
 import java.util.Objects;
 
 public class Plain {
-    public static String formatter(List<Map<String, Object>> data) throws Exception {
+    public static String formatter(List<Map<String, Object>> data) {
 
         var result = new StringBuilder();
 
