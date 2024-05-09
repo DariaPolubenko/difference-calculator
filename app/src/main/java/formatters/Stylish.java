874f6817f8deca1ad28bf06ea5c454a1f370bb74
Daria Polubenko <daria.polubenko@gmail.com>
2024-05-10 00:59:55 +0700

    update Stylish.java

--- app/src/main/java/formatters/Stylish.java
+++ app/src/main/java/formatters/Stylish.java
@@ -10,18 +10,19 @@ public class Stylish {
         result.append("{\n");
 
         for (var map : data) {
+            var value = map.get("value");
             var value1 = map.get("value1");
             var value2 = map.get("value2");
 
             if (Objects.equals(map.get("type"), "unupdated")) {
-                result.append("    " + map.get("key") + ": " + value1 + "\n");
+                result.append("    " + map.get("key") + ": " + value + "\n");
             } else if (Objects.equals(map.get("type"), "updated")) {
                 result.append("  - " + map.get("key") + ": " + value1 + "\n");
                 result.append("  + " + map.get("key") + ": " + value2 + "\n");
             } else if (Objects.equals(map.get("type"), "added")) {
-                result.append("  + " + map.get("key") + ": " + value2 + "\n");
+                result.append("  + " + map.get("key") + ": " + value + "\n");
             } else if (Objects.equals(map.get("type"), "removed")) {
-                result.append("  - " + map.get("key") + ": " + value1 + "\n");
+                result.append("  - " + map.get("key") + ": " + value + "\n");
             }
         }
         result.append("}");
