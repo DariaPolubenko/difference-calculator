37796816c1500be8685075af4a0f0ca9b87e95e1
Daria Polubenko <daria.polubenko@gmail.com>
2024-05-10 01:00:27 +0700

    update Content.java

--- app/src/main/java/hexlet/code/Content.java
+++ app/src/main/java/hexlet/code/Content.java
@@ -1,2 +1,16 @@
-package hexlet.code;public class Content {
+package hexlet.code;
+
+import java.util.Map;
+
+public class Content {
+    public static Map<String, Object> formatContent(String filepath, String content) throws Exception {
+        if (filepath.lastIndexOf("json") > 0) {
+            return Parser.parseJson(content);
+        } else if (filepath.lastIndexOf("yml") > 0) {
+            return Parser.parseYaml(content);
+        } else {
+            System.out.println("Don't have this format");
+        }
+        return null;
+    }
 }
