0754686a60556a8f9f8360d468d035e7c2880824
Daria Polubenko <daria.polubenko@gmail.com>
2024-05-09 22:58:52 +0700

    update Differ.java

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,15 +1,12 @@
 package hexlet.code;
 
 import static hexlet.code.Formatter.formatter;
+import static hexlet.code.Tree.getTree;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.HashMap;
-import java.util.TreeMap;
-import java.util.Objects;
 
 public class Differ {
     public static String generate(String filepath1,  String filepath2) throws Exception {
@@ -23,7 +20,7 @@ public class Differ {
         var data1 = getData(filepath1);
         var data2 = getData(filepath2);
 
-        List<Map<String, Object>> differences = getDifference(data1, data2);
+        List<Map<String, Object>> differences = getTree(data1, data2);
         return formatter(differences, format);
     }
 
@@ -45,57 +42,5 @@ public class Differ {
                 System.out.println("Don't have this format: \"" + format[1] + "\"");
         }
         return null;
-    }
-
-    public static List<Map<String, Object>> getDifference(Map<String, Object> data1, Map<String, Object> data2) {
-        var unitedKeys = new TreeMap<>(data2);
-        data1.forEach((key, value) -> {
-            unitedKeys.computeIfAbsent(key, (v) -> value);
-        });
-
-        var result = new ArrayList<Map<String, Object>>();
-
-        unitedKeys.forEach((key, value) -> {
-            var helperMap = new HashMap<String, Object>();
-            var value1 = data1.get(key);
-            var value2 = data2.get(key);
-
-            if (key.equals("")) {
-                result.add(Map.of("", ""));
-
-            } else if (data1.containsKey(key) && data2.containsKey(key)) {
-                if (Objects.equals(value1, value2)) {
-                    helperMap.put("key", key);
-                    helperMap.put("type", "unupdated");
-                    helperMap.put("value1", value1);
-                    helperMap.put("value2", value1);
-
-                    result.add(helperMap);
-                } else {
-                    helperMap.put("key", key);
-                    helperMap.put("type", "updated");
-                    helperMap.put("value1", value1);
-                    helperMap.put("value2", value2);
-
-                    result.add(helperMap);
-                }
-            } else if (!data1.containsKey(key)) {
-                helperMap.put("key", key);
-                helperMap.put("type", "added");
-                helperMap.put("value1", "");
-                helperMap.put("value2", value2);
-
-                result.add(helperMap);
-
-            } else if (!data2.containsKey(key)) {
-                helperMap.put("key", key);
-                helperMap.put("type", "removed");
-                helperMap.put("value1", value1);
-                helperMap.put("value2", "");
-
-                result.add(helperMap);
-            }
-        });
-        return result;
     }
 }
