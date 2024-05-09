e75550e30eac9cc8dc5659c27592723375697932
Daria Polubenko <daria.polubenko@gmail.com>
2024-05-10 03:42:33 +0700

    update Differ.java

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -15,9 +15,6 @@ public class Differ {
     }
 
     public static String generate(String filepath1,  String filepath2, String format) throws Exception {
-        //filepath1 = filepath1.contains("src/test/resources/") ? filepath1 : "src/test/resources/" + filepath1;
-        //filepath2 = filepath2.contains("src/test/resources/") ? filepath2 : "src/test/resources/" + filepath2;
-
         var data1 = getData(filepath1);
         var data2 = getData(filepath2);
 
