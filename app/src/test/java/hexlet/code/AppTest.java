ad7de4b5e04a107ee9eaa2959083cf1e5c582198
Daria Polubenko <daria.polubenko@gmail.com>
2024-05-16 03:30:00 +0700

    update AppTest.java

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -7,53 +7,47 @@ class AppTest {
     @Test
     public void test1() throws Exception {
         var actual = Differ.generate("src/test/resources/file1.json", "src/test/resources/file2.json");
-        var expected = "{\n"
-                       + "  - follow: false\n"
-                       + "    host: hexlet.io\n"
-                       + "  - proxy: 123.234.53.22\n"
-                       + "  - timeout: 50\n"
-                       + "  + timeout: 20\n"
-                       + "  + verbose: true\n"
-                       + "}";
+        var expected = getExpectedDefault1();
         assertEquals(expected, actual);
     }
 
     @Test
     public void test2() throws Exception {
-        var actual = Differ.generate("src/test/resources/file3.json", "src/test/resources/file1.json");
-        var expected = "{\n"
-                + "  + follow: false\n"
-                + "  + host: hexlet.io\n"
-                + "  + proxy: 123.234.53.22\n"
-                + "  + timeout: 50\n"
-                + "}";
+        var actual = Differ.generate("src/test/resources/file31.yml", "src/test/resources/file32.yml");
+        var expected = getExpectedDefault2();
         assertEquals(expected, actual);
     }
 
     @Test
     public void test3() throws Exception {
-        var actual = Differ.generate("src/test/resources/file1.json", "src/test/resources/file3.json");
-        var expected = "{\n"
-                + "  - follow: false\n"
-                + "  - host: hexlet.io\n"
-                + "  - proxy: 123.234.53.22\n"
-                + "  - timeout: 50\n"
-                + "}";
+        var actual = Differ.generate("src/test/resources/file31.json", "src/test/resources/file32.json", "plain");
+        var expected = getExpectedPlain();
         assertEquals(expected, actual);
     }
 
     @Test
     public void test4() throws Exception {
-        var actual = Differ.generate("src/test/resources/file3.json", "src/test/resources/file4.json");
-        var expected = "{\n"
-                + "}";
+        var actual = Differ.generate("src/test/resources/file31.yml", "src/test/resources/file32.yml", "plain");
+        var expected = getExpectedPlain();
         assertEquals(expected, actual);
     }
 
     @Test
     public void test5() throws Exception {
-        var actual = Differ.generate("src/test/resources/file1.yml", "src/test/resources/file2.yml");
-        var expected = "{\n"
+        var actual = Differ.generate("src/test/resources/file31.json", "src/test/resources/file32.json", "json");
+        var expected = getExpectedJson();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test6() throws Exception {
+        var actual = Differ.generate("src/test/resources/file31.yml", "src/test/resources/file32.yml", "json");
+        var expected = getExpectedJson();
+        assertEquals(expected, actual);
+    }
+
+    public static String getExpectedDefault1() {
+        return "{\n"
                 + "  - follow: false\n"
                 + "    host: hexlet.io\n"
                 + "  - proxy: 123.234.53.22\n"
@@ -61,14 +55,10 @@ class AppTest {
                 + "  + timeout: 20\n"
                 + "  + verbose: true\n"
                 + "}";
-        assertEquals(expected, actual);
     }
 
-    @Test
-    public void test6() throws Exception {
-        var actual = Differ.generate("src/test/resources/file31.json", "src/test/resources/file32.json");
-
-        var expected = "{\n"
+    public static String getExpectedDefault2() {
+        return "{\n"
                 + "    chars1: [a, b, c]\n"
                 + "  - chars2: [d, e, f]\n"
                 + "  + chars2: false\n"
@@ -93,66 +83,9 @@ class AppTest {
                 + "  - setting3: true\n"
                 + "  + setting3: none\n"
                 + "}";
-        assertEquals(expected, actual);
     }
-
-    @Test
-    public void test7() throws Exception {
-        var actual = Differ.generate("src/test/resources/file31.yml", "src/test/resources/file32.yml");
-
-        var expected = "{\n"
-                + "    chars1: [a, b, c]\n"
-                + "  - chars2: [d, e, f]\n"
-                + "  + chars2: false\n"
-                + "  - checked: false\n"
-                + "  + checked: true\n"
-                + "  - default: null\n"
-                + "  + default: [value1, value2]\n"
-                + "  - id: 45\n"
-                + "  + id: null\n"
-                + "  - key1: value1\n"
-                + "  + key2: value2\n"
-                + "    numbers1: [1, 2, 3, 4]\n"
-                + "  - numbers2: [2, 3, 4, 5]\n"
-                + "  + numbers2: [22, 33, 44, 55]\n"
-                + "  - numbers3: [3, 4, 5]\n"
-                + "  + numbers4: [4, 5, 6]\n"
-                + "  + obj1: {nestedKey=value, isNested=true}\n"
-                + "  - setting1: Some value\n"
-                + "  + setting1: Another value\n"
-                + "  - setting2: 200\n"
-                + "  + setting2: 300\n"
-                + "  - setting3: true\n"
-                + "  + setting3: none\n"
-                + "}";
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void test8() throws Exception {
-        var actual = Differ.generate("src/test/resources/file31.json", "src/test/resources/file32.json", "plain");
-
-        var expected = "Property 'chars2' was updated. From [complex value] to false\n"
-                     + "Property 'checked' was updated. From false to true\n"
-                     + "Property 'default' was updated. From null to [complex value]\n"
-                     + "Property 'id' was updated. From 45 to null\n"
-                     + "Property 'key1' was removed\n"
-                     + "Property 'key2' was added with value: 'value2'\n"
-                     + "Property 'numbers2' was updated. From [complex value] to [complex value]\n"
-                     + "Property 'numbers3' was removed\n"
-                     + "Property 'numbers4' was added with value: [complex value]\n"
-                     + "Property 'obj1' was added with value: [complex value]\n"
-                     + "Property 'setting1' was updated. From 'Some value' to 'Another value'\n"
-                     + "Property 'setting2' was updated. From 200 to 300\n"
-                     + "Property 'setting3' was updated. From true to 'none'";
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void test9() throws Exception {
-        var actual = Differ.generate("src/test/resources/file31.yml", "src/test/resources/file32.yml", "plain");
-
-        var expected = "Property 'chars2' was updated. From [complex value] to false\n"
+    public static String getExpectedPlain() {
+        return "Property 'chars2' was updated. From [complex value] to false\n"
                 + "Property 'checked' was updated. From false to true\n"
                 + "Property 'default' was updated. From null to [complex value]\n"
                 + "Property 'id' was updated. From 45 to null\n"
@@ -165,14 +98,10 @@ class AppTest {
                 + "Property 'setting1' was updated. From 'Some value' to 'Another value'\n"
                 + "Property 'setting2' was updated. From 200 to 300\n"
                 + "Property 'setting3' was updated. From true to 'none'";
-        assertEquals(expected, actual);
     }
 
-    @Test
-    public void test10() throws Exception {
-        var actual = Differ.generate("src/test/resources/file31.json", "src/test/resources/file32.json", "json");
-
-        var expected = "[ {\n"
+    public static String getExpectedJson() {
+        return "[ {\n"
                 + "  \"type\" : \"unupdated\",\n"
                 + "  \"value\" : [ \"a\", \"b\", \"c\" ],\n"
                 + "  \"key\" : \"chars1\"\n"
@@ -244,6 +173,5 @@ class AppTest {
                 + "  \"type\" : \"updated\",\n"
                 + "  \"key\" : \"setting3\"\n"
                 + "} ]";
-        assertEquals(expected, actual);
     }
 }
